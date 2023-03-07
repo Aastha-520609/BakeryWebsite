c22c1fea096382c2029d8bbc4cc1c20934f0b8f6
Aastha-520609 <aasthachaudhary5050@gmail.com>
2023-03-07 22:12:03 +0530

    added some style

--- src/components/PopularItem.jsx
+++ src/components/PopularItem.jsx
@@ -2,11 +2,12 @@
 
 import { useEffect, useState} from "react";
 import styled from "styled-components";
-import {Splide, SplideSlide } from '@splidejs/react-splide';
-import "@splidejs/splide/dist/css/splide.min.css";
+import { Splide,SplideTrack,SplideSlide } from "@splidejs/react-splide";
+//import "@splidejs/splide/dist/css/splide.min.css";
+import '@splidejs/react-splide/css';
 
 //usestate is used to save the data
-function Popular() {
+function Popular() {  
   const [popular, setPopular] = useState([]);
   //to get the getPopular function run soon as component is rendered, we use (use Effect)
    useEffect(() =>{
@@ -33,18 +34,19 @@ function Popular() {
              options={{
                perPage: 4,
                //arrows: false,
-               //pagination: false,
-               //drag: "free",
-               //gap: "5rem",
+               pagination: false,
+               drag: 'free',
+               gap: "3rem",
             }} 
           >
            {popular.map((recipe) => //loop through all the recipe on the api
            {
              return(
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
               <Card>
                   <p>{recipe.title}</p>
-                  <img src={recipe.image} alt="recipe.title" />
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
               </Card>
               </SplideSlide>
              );
@@ -62,14 +64,47 @@ const Wrapper = styled.div`
 
 const Card = styled.div`
 
- min-height: 25rem;
- border-radius: 2rem;
- overflow: hidden;
-
- img{
+  min-height: 15rem;
   border-radius: 2rem;
- }
+  overflow: hidden;
+  position: relative;
 
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-context: center;
+    align-items: center;
+    
+  }
+`;
+const Gradient = styled.div`{
+  z-index: 3;
+  position: absoulte;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+
+}
+   
 `;
 
 export default Popular;
