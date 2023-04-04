cfdfcb78d2ee2d63d0e446dfc7398773e2df571b
Aastha-520609 <aasthachaudhary5050@gmail.com>
2023-04-04 23:27:13 +0530

    added logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import VegItem from "../components/VegItem";
 import PopularItem from "../components/PopularItem"; 
+import { motion } from "framer-motion";
 
 import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <motion.div
+    animate= {{ opacity: 1}}
+    initial= {{ opacity: 0}}
+    exit={{ opacity: 0}}
+    transition={{ duration: 0.5}}
+    >
       <VegItem />
       <PopularItem />
-    </div>
+    </motion.div>
   );
 }
 
